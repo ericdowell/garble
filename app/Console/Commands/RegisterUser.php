c2b7f0fc812efd968398d8100db5af1f5e6c6de0
Eric Dowell <github@ericdowell.com>
2018-06-03 14:35:05 -0500

    Updating RegisterUser before moving it.

--- app/Console/Commands/RegisterUser.php
+++ app/Console/Commands/RegisterUser.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Garble\Console\Commands;
 
-use Garble\User;
+use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
 
@@ -13,43 +15,65 @@ class RegisterUser extends Command
      *
      * @var string
      */
-    protected $signature = 'register:user';
+    protected $signature = 'register:user {--M|model=}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Register a new user account';
+    protected $description = 'Register a new user account.';
+
+    /**
+     * Get the Laravel application instance.
+     *
+     * @return string
+     * @throws \Exception
+     */
+    public function getUserModelClassName(): string
+    {
+        /** @var \Illuminate\Foundation\Application $laravel */
+        $laravel = $this->laravel;
+        $fallback = $laravel->getNamespace().'\\User';
+
+        $userModel = $this->option('model') ?? config('auth.providers.users.model', $fallback);
+        if (! class_exists($userModel)) {
+            throw new Exception($userModel.' does not exist.');
+        }
+
+        return $userModel;
+    }
 
     /**
      * Execute the console command.
      *
      * @return mixed
+     * @throws \Exception
      */
-    public function handle()
+    public function handle(): int
     {
         $attributes = [];
-        $attributes['name'] = $this->ask('Enter name:');
+        $userModel = $this->getUserModelClassName();
+        $attributes['name'] = $this->ask('Enter name');
 
         $this->askForEmail($attributes);
 
         $inputUsername = $this->confirm('Do you want to set a username?');
         if ($inputUsername) {
-            $attributes['username'] = $this->ask('Enter in username:');
+            $attributes['username'] = $this->ask('Enter in username');
         }
 
         $this->askForPassword($attributes);
 
-        $created = User::create($attributes);
+        $created = forward_static_call([$userModel, 'create'], $attributes);
         if ($created) {
             $outputSafe = array_except($attributes, ['password']);
-            $this->info('User created!');
+            $this->info('User successfully created!');
             $this->table(array_keys($outputSafe), [array_values($outputSafe)]);
 
             return 0;
         }
-        $this->info('Sorry, user was not created. Please try again.');
+        $this->info('Sorry, user was NOT created. Please try again.');
 
         return 1;
     }
@@ -59,7 +83,7 @@ class RegisterUser extends Command
      */
     protected function askForEmail(array &$attributes)
     {
-        $email = $this->ask('Enter in an email:');
+        $email = $this->ask('Enter in an email');
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $attributes['email'] = $email;
 
@@ -75,14 +99,14 @@ class RegisterUser extends Command
      */
     protected function askForPassword(array &$attributes)
     {
-        $password = $this->secret('Enter password:');
-        $confirm = $this->secret('Confirm password:');
+        $password = $this->secret('Enter password');
+        $confirm = $this->secret('Confirm password');
         if ($password === $confirm) {
             $attributes['password'] = Hash::make($password);
 
             return;
         }
-        $this->info('The password and confirmation password don\'t match, please try again.');
+        $this->info('The password and confirmation password do NOT match, please try again.');
 
         $this->askForPassword($attributes);
     }
