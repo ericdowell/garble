532eebcb5278b718b5ae1f0472634d2b73f0f5a1
Eric Dowell <eric.dowell@startribune.com>
2018-09-26 02:14:54 +0000

    Adding check for DB_DEFAULT_STRING_LENGTH. (#41)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace Garble\Providers;
 use Garble\Note;
 use Garble\Post;
 use Garble\ToDo;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
 
@@ -17,6 +18,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (is_numeric(env('DB_DEFAULT_STRING_LENGTH'))) {
+            Schema::defaultStringLength(env('DB_DEFAULT_STRING_LENGTH'));
+        }
         Relation::morphMap([
             'note' => Note::class,
             'post' => Post::class,
